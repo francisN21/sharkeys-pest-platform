84126b5899af91395cb49c3db6a932c58c4a7679
Francisco Rones <ininorones@gmail.com>
2026-02-05 23:53:15 -0800

    Delete backend/auth/auth_backup directory

--- backend/auth/auth_backup/__tests__/helpers/dbReset.js
+++
@@ -1,9 +0,0 @@
-const { pool } = require("../../src/db");
-
-async function resetDb() {
-  await pool.query(
-    "TRUNCATE TABLE email_tokens, sessions, oauth_identities, users RESTART IDENTITY CASCADE"
-  );
-}
-
-module.exports = { resetDb };
--- backend/auth/auth_backup/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.test.js", "**/log/**/*.test.js"],
-  verbose: true,
-};
