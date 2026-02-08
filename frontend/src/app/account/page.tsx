4aed2777b4ce1ac443cb2b527150cceeb424d416
Francisco Rones (T2 Las Vegas) <francisco.rones@take2games.com>
2026-02-07 23:50:29 -0800

    Added Navbar to account page

--- frontend/src/app/account/page.tsx
+++ frontend/src/app/account/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import Navbar from "../../components/Navbar";
 import Profilpage from "../account/profile/page";
 import Bookingspgae from "../account/bookings/page";
 
@@ -11,31 +12,34 @@ export default function HomePage() {
   const [activeSection, setActiveSection] = useState<Section>("one");
 
   return (
-    <main className="min-h-screen bg-gray-100 flex flex-col items-center p-6">
-      {/* Tabs */}
-      <div className="flex gap-3 mb-8">
-        <TabButton
-          label="Page 1"
-          active={activeSection === "one"}
-          onClick={() => setActiveSection("one")}
-        />
-        <TabButton
-          label="Page 2"
-          active={activeSection === "two"}
-          onClick={() => setActiveSection("two")}
-        />
-        <TabButton
-          label="Page 3"
-          active={activeSection === "three"}
-          onClick={() => setActiveSection("three")}
-        />
-      </div>
+    <main className="h-screen overflow-y-auto scroll-smooth md:snap-y md:snap-mandatory">
+        <Navbar />
+            <section className="mx-auto max-w-3xl px-4 py-10 space-y-6">
+                {/* Tabs */}
+                <div className="flex gap-3 mb-8">
+                    <TabButton
+                    label="Page 1"
+                    active={activeSection === "one"}
+                    onClick={() => setActiveSection("one")}
+                    />
+                    <TabButton
+                    label="Page 2"
+                    active={activeSection === "two"}
+                    onClick={() => setActiveSection("two")}
+                    />
+                    <TabButton
+                    label="Page 3"
+                    active={activeSection === "three"}
+                    onClick={() => setActiveSection("three")}
+                    />
+                </div>
 
-      {/* Render modular "pages" */}
-      <div className="w-full max-w-3xl bg-white rounded-xl shadow p-6">
-        {activeSection === "one" && <Profilpage />}
-        {activeSection === "two" && <Bookingspgae />}
-      </div>
+                {/* Render modular "pages" */}
+                <div className="w-full max-w-3xl bg-white rounded-xl shadow p-6">
+                    {activeSection === "one" && <Profilpage />}
+                    {activeSection === "two" && <Bookingspgae />}
+                </div>
+            </section>
     </main>
   );
 }
