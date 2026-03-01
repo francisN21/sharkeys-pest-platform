d6b06265fd1e34cc4c3ee8b28163a65f84015a3e
francisN21 <ininorones@gmail.com>
2026-02-28 21:27:51 -0800

    Added fontawesome in the root layout to ensure all icons are being rendered

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -21,6 +21,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+      />
       <body>
         <AuthProvider>
           <ThemeProvider>{children}</ThemeProvider>
