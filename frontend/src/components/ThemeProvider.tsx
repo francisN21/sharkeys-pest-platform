7d0f868c128ea236f6e631e0015edceb15b71765
francisN21 <ininorones@gmail.com>
2026-03-28 08:06:10 -0700

    fix(theme): set default theme to dark, disable system preference override

--- frontend/src/components/ThemeProvider.tsx
+++ frontend/src/components/ThemeProvider.tsx
@@ -6,8 +6,8 @@ export default function ThemeProvider({ children }: { children: React.ReactNode 
   return (
     <NextThemesProvider
       attribute="data-theme"
-      defaultTheme="system"
-      enableSystem
+      defaultTheme="dark"
+      enableSystem={false}
       disableTransitionOnChange
     >
       {children}
